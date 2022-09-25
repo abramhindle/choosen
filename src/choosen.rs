af2b836594a0add623c3a96310106e17d9792b24
Abram Hindle <abram.hindle@softwareprocess.es>
2022-09-25 10:09:09 -0600

    Ignore UTF-8 decoding errors

--- src/choosen.rs
+++ src/choosen.rs
@@ -3,26 +3,38 @@ use std::collections::BinaryHeap;
 use std::{io, io::prelude::*};
 use rand::prelude::*;
 
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
         panic!("Need at least 1 argument")
     }
+
     let n = args[1].parse::<usize>().unwrap();
     // std::println!("{:?}", args);
     let mut rng = rand::thread_rng();
     let mut heap = BinaryHeap::new();
     let fd = io::stdin();
+    let mut lines = 0;
+    let mut errs = 0;
     for line in fd.lock().lines() {
-        let uline = line.unwrap();
-        let r: u64 = rng.gen();
-        // println!("{}", uline);
-        heap.push((r,uline));
-        while heap.len() > n {
-            heap.pop();
+        lines += 1;
+        match line {
+            Ok(uline) => {
+                // let uline = line.unwrap();
+                let r: u64 = rng.gen();
+                // println!("{}", uline);
+                heap.push((r,uline));
+                while heap.len() > n {
+                    heap.pop();
+                }
+            },
+            Err(e) => {
+                errs += 1;
+                eprintln!("{}", e);
+            }
         }
     }
+    eprintln!("Lines:{} Errors:{}", lines, errs);
     while heap.len() > n {
         heap.pop();
     }
